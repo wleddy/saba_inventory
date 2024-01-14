3e0b8ca6bf992938b36bc9b1fbc34003bfa56c8c
Bill Leddy <bill@williesworkshop.net>
2024-01-13 16:37:02 -0800

    Update to be compatible with Flask 3

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
-""" This is the start of Flask Application
-
-This module handles initialization, setup and common request routing
-for the Flask application
-
-Args:
-    None
-
-Returns:
-    None
-
-Raises:
-    None
-"""
+from flask import g, session, request, redirect, abort, url_for
 import os
-
-from flask import g, session, request, redirect, flash, abort, url_for
-from flask_mail import Mail
 from shotglass2 import shotglass
 from shotglass2.takeabeltof.database import Database
 from shotglass2.takeabeltof.jinja_filters import register_jinja_filters
@@ -27,20 +11,35 @@ from inventory.views import item
 
 # Create app
 import logging 
-try:
-    app = shotglass.create_app(
-            __name__,
-            instance_path='../data_store/instance',
-            config_filename='site_settings.py',
-            static_folder=None,
-            )
-except:
-    logging.exception('')
 
-@app.before_first_request
-def start_logging():
+app = shotglass.create_app(
+        __name__,
+        instance_path='../data_store/instance',
+        config_filename='site_settings.py',
+        static_folder=None,
+        )
+        
+def start_app():
     shotglass.start_logging(app)
-    shotglass.start_backup_thread(os.path.normpath(os.path.join(app.root_path,shotglass.get_site_config()['DATABASE_PATH'])))
+    initalize_base_tables()
+    ## Setup the routes for users
+    shotglass.register_users(app)
+
+    # setup www.routes...
+    shotglass.register_www(app)
+
+    app.register_blueprint(tools.mod)
+    
+    register_blueprints() # Register all the other bluepints for the app
+
+    # use os.path.normpath to resolve true path to data file when using '../' shorthand
+    shotglass.start_backup_thread(
+        os.path.normpath(
+            os.path.join(
+                app.root_path,shotglass.get_site_config()['DATABASE_PATH']
+                )
+            )
+        )
 
 
 @app.context_processor
@@ -48,52 +47,36 @@ def inject_site_config():
     # Add 'site_config' dict to template context
     return {'site_config':shotglass.get_site_config()}
 
-
-# work around some web servers that mess up root path
-# from werkzeug.contrib.fixers import CGIRootFix
-# if app.config['CGI_ROOT_FIX_APPLY'] == True:
-#     fixPath = app.config.get("CGI_ROOT_FIX_PATH","/")
-#     app.wsgi_app = CGIRootFix(app.wsgi_app, app_root=fixPath)
-
 register_jinja_filters(app)
-inventory.register_jinja_filters(app)
 
-mail = Mail(app)
 
-def init_db(db=None):
-    # to support old code
-    initalize_all_tables(db)
-
-def initalize_all_tables(db=None):
-    """Place code here as needed to initialze all the tables for this site"""
-    if not db:
-        db = get_db()
-        
-    shotglass.initalize_user_tables(db)
-    
-    ### setup any other tables you need here....
-    inventory.initalize_tables(db)
-    
 def get_db(filespec=None):
     """Return a connection to the database.
+
     If the db path does not exist, create it and initialize the db"""
-    
+
     if not filespec:
         filespec = shotglass.get_site_config()['DATABASE_PATH']
-        
+
     # This is probobly a good place to change the
     # filespec if you want to use a different database
     # for the current request.
+
+    # test the path, if not found, try to create it
+    if shotglass.make_db_path(filespec):
+        g.db = Database(filespec).connect()
+        initalize_base_tables(g.db)
     
-        
-    # test the path, if not found, create it
-    initialize = shotglass.make_db_path(filespec)
-        
-    g.db = Database(filespec).connect()
-    if initialize:
-        initalize_all_tables(g.db)
-            
-    return g.db
+        return g.db
+    else:
+        # was unable to create a path to the database
+        raise IOError("Unable to create path to () in app.get_db".format(filespec))
+
+    
+@app.context_processor
+def inject_site_config():
+    # Add 'site_config' dict to template context
+    return {'site_config':shotglass.get_site_config()}
 
 
 @app.before_request
@@ -104,12 +87,14 @@ def _before():
 
     #ensure that nothing is served from the instance directory
     if 'instance' in request.url:
-        abort(404)
+        return abort(404)
         
-    #import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
+    # print(app.url_map)
+    session.permanent = True
     
-    shotglass.get_app_config(app)
-    shotglass.set_template_dirs(app) #g.template_list is now a list of templates
+    shotglass.get_site_config(app)
+    shotglass.set_template_dirs(app)
     
     get_db()
     session.permanent = True
@@ -153,30 +138,44 @@ def page_not_found(error):
 def server_error(error):
     return shotglass.server_error(error)
 
+
+def initalize_base_tables(db=None):
+    """Place code here as needed to initialze all the tables for this site"""
+    if not db:
+        db = get_db()
+    
+    shotglass.initalize_user_tables(db)
+
+    # ### setup any other tables you need here....
+    # import starter_module.models
+    # starter_module.models.init_db(db)
+    
+def register_blueprints():
+    """Register all your blueprints here and initialize 
+    any data tables they need.
+    """
+    # # add app specific modules...
+    # Setup inventory
+    inventory.register_blueprints(app)
+
+
 #Register the static route
 app.add_url_rule('/static/<path:filename>','static',shotglass.static)
 
 #Register the home page
 app.add_url_rule('/','display',item.display)
 
-## Setup the routes for users
-shotglass.register_users(app)
+# ## Setup the routes for users
+# shotglass.register_users(app)
 
-# setup www.routes...
-shotglass.register_www(app)
+with app.app_context():
+    start_app()
 
-# Setup inventory
-inventory.register_blueprints(app)
 
-app.register_blueprint(tools.mod)
+# app.register_blueprint(tools.mod)
 
 if __name__ == '__main__':
-    
-    with app.app_context():
-        # create the default database if needed
-        initalize_all_tables()
-        
-    #app.run(host='localhost', port=8000)
-    app.run(host='inventory.willie.local')
+    app.run(host='inventory.willie.local', port=5000)
+    #app.run()
     
     
